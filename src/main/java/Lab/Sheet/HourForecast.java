e9c8e3be85774c403097710c65d8617d028d6d14
Energy-Drink <Kzaksauskasss@gmail.com>
2022-11-01 18:07:07 +0000

    Second Push

---
+++ src/main/java/Lab/Sheet/HourForecast.java
@@ -0,0 +1,14 @@
+package Lab.Sheet;
+
+public class HourForecast {
+
+    private double temperature;
+
+    public double getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(double temperature) {
+        this.temperature = temperature;
+    }
+}
